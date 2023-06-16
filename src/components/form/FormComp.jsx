2b9f0df70975df9174d7f9f3254f99128f483cd2
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-16 16:04:11 +0530

    home page created, formComp created

---
+++ src/components/form/FormComp.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import success from '../../assests/img/icon-success.svg';
+
+const FormComp = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Stay updated!</h1>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        </div>
+        <div>
+          <div>
+            <img src={success} alt='' />
+            <p>Product discovery and building what matters</p>
+          </div>
+          <div>
+            <img src={success} alt='' />
+            <p>Measuring to ensure updates are a success</p>
+          </div>
+          <div>
+            <img src={success} alt='' />
+            <p> And much more!</p>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default FormComp;
